df0f7c237fe18284e063823fdc702d3f118b3574
majda <majdaelfadil4@gmail.com>
2023-05-04 08:06:23 +0200

    first commit

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+let subMenu = document.getElementById("subMenu");
+
+function showMenu() {
+    subMenu.classList.toggle("open-menu")
+}
+function hidMenu() {
+    submenu.classList.toggle("hide-menu")
+}
+
